bd62f38e4c1b6842c2df4da8b2411694efb9ab14
Matthew McLimans <mmclimans@paloaltonetworks.com>
2021-11-01 23:13:08 -0400

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -2,3 +2,7 @@ output "API_SERVER_ADDRESS" {
   description = "The IP endpoint of the GKE cluster master.  Use this value in the Kubernetes Panorama plugin for the API Server Address"
   value = google_container_cluster.cluster.endpoint
 }
+
+output "cluster_name" {
+  value = ${google_container_cluster.cluster.name}
+}
